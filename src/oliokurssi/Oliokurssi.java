0de90307e7e13c1d830edabb787daf7484fbcdb1
Cyberpunkd <Cyberpunkd@192.168.0.12>
2018-07-20 22:14:03 +0300

    Fist commit

---
+++ src/oliokurssi/Oliokurssi.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oliokurssi;
+
+/**
+ *
+ * @author Cyberpunkd
+ */
+public class Oliokurssi {
+
+    /**
+     * @param args the command line arguments
+     */
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        
+        int i=0;        
+
+        do{
+        i++;
+        System.out.println(i);
+        } while (i<10);
+        
+        tulo 
+    }
+    
+    public int tulo (int kerroin, int kerrottava){
+          return (kerroin * kerrottava);
+        
+    }
+
+    
+}
